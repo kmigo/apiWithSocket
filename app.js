eb44d217b5f3e6feab8595889049fab13e1539a6
Patrick <tk_patrick@hotmail.com>
2021-09-10 22:52:57 -0300

    novo

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const expres = require('express')
 const app = expres()
+const bodyPars = require('body-parser');
 const io = require("socket.io-client")("http://apiwebsocket.herokuapp.com/",{
   transports: ["websocket"] //
 })
 
+app.use(bodyPars.urlencoded({ extended: false }));
+app.use(bodyPars.json());
 io.on("connection", (socket) => {
   console.log('logged');
 })
@@ -16,5 +19,16 @@ app.get('/',(req,res,next) => {
       mensagem:"ok"
   });
 })
+
+app.post('/send',(req,res,next) => {
+  console.log(req.body)
+  io.emit('distribution',{
+    event:req.body.event
+    ,data:req.body.data})
+  res.status(200).send({
+      mensagem:"ok"
+  });
+})
+
 
 module.exports = app;
